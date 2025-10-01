07e064edeb2803d5a0cf5a6b1345704a34a6f315
Akruti Gupta <akruti.gupta.cs22@ggits.net>
2025-10-01 15:31:23 +0530

    2 Flights commit

--- myapp/classes0.py
+++ myapp/classes0.py
@@ -5,18 +5,32 @@ class Flight:
         self.destination = destination
         self.duration = duration
 
+    def print_info(self):
+        print(f"Flight origin: {self.origin}")
+        print(f"Flight destination: {self.destination}")
+        print(f"Flight duration: {self.duration}")
+
 def main():
 
     #create flight
-    f = Flight(origin="New York", destination="Paris", duration=540)
+    #f = Flight(origin="New York", destination="Paris", duration=540)
+
+    #we have 2 flights info and want to print
+    f1 = Flight(origin="New York", destination="Paris", duration="540")
+    f1.print_info()
+
+    f2 = Flight(origin="Tokyo", destination="Shangai", duration="185")
+    f2.print_info()
 
     #change the value of a variable
-    f.duration += 10
+    #f.duration += 10
 
     #print detail about flight
-    print(f.origin)
-    print(f.destination)
-    print(f.duration)
+    # print(f.origin)
+    # print(f.destination)
+    # print(f.duration)
+
+    
 
 if __name__ == "__main__":
     main()
