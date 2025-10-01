5428146ee621c8371f901f8376fa5ad186ea34ee
Akruti Gupta <akruti.gupta.cs22@ggits.net>
2025-10-01 15:07:40 +0530

    Flight0 intinal commit

---
+++ myapp/classes0.py
@@ -0,0 +1,22 @@
+class Flight:
+
+    def __init__(self, origin, destination, duration):
+        self.origin = origin
+        self.destination = destination
+        self.duration = duration
+
+def main():
+
+    #create flight
+    f = Flight(origin="New York", destination="Paris", duration=540)
+
+    #change the value of a variable
+    f.duration += 10
+
+    #print detail about flight
+    print(f.origin)
+    print(f.destination)
+    print(f.duration)
+
+if __name__ == "__main__":
+    main()
